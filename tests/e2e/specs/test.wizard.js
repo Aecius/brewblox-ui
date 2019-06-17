c44bfdf196586393588f4993fde9b94f8bbb8433
Frank van Hal <frank@localhost.localdomain>
2019-06-17 14:05:13 +0200

    Started work on some commands for future testing.

--- tests/e2e/specs/test.wizard.js
+++ tests/e2e/specs/test.wizard.js
@@ -2,8 +2,7 @@
 
 describe('Wizard tests', () => {
   it('Restarts the backend', () => {
-    cy.exec('npm run couchdb:docs');
-    cy.exec('npm run spark:blocks');
+    cy.resetSpark();
   });
 
   it('Creates a new SetpointSensorPair', () => {
@@ -22,7 +21,7 @@ describe('Wizard tests', () => {
     cy.contains(/NEXT$/i).click();
 
     cy.contains(/CREATE NEW BLOCK$/i).click();
-    cy.wait(10);
+    cy.wait(11);
 
     cy.contains(/CONFIGURE BLOCK$/i).should('be.disabled');
     cy.contains(/CREATE$/i).should('be.disabled');
--- tests/e2e/support/commands.js
+++ tests/e2e/support/commands.js
@@ -23,3 +23,25 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('resetSpark', () => {
+  cy.exec('npm run couchdb:docs');
+  cy.exec('npm run spark:blocks');
+});
+
+
+ypress.Commands.add('MakeWidget', (type) => {
+  // Look into making it check if the menu is open before clicking it.
+  //  cy.contains('menu').click();
+  cy.contains('Wizardry').click();
+  cy.contains('Widget').click();
+
+  cy.contains(/NEXT$/i).should('be.disabled');
+
+  cy.get('.q-select__input').click();
+  cy.contains(type).click();
+  cy.contains(/NEXT$/i).click();
+
+  cy.contains(/CREATE NEW BLOCK$/i).click();
+  cy.wait(10);
+});
